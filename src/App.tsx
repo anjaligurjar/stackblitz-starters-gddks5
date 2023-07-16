408f851b9888f05e1ea320c839ff0396bc4cdba4
anjaligurjar <anjaligurjar861@gmail.com>
2023-07-16 12:37:51 +0530

    1 file modified

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import * as React from 'react';
+import Login from './Componenets/Login'
 import './style.css';
 
 export default function App() {
   return (
     <div>
-     
+     <Login />
     </div>
   );
 }
